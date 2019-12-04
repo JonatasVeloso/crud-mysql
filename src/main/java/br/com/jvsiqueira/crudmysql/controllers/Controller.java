8dcffa0511347ef87f5a8bc3ee15aec1fa3fffd3
Jonatas Veloso Siqueira <jvelosos@indracompany.com>
2019-12-04 11:17:11 -0300

    :rocket: Adicionado Thymeleaf

--- src/main/java/br/com/jvsiqueira/crudmysql/controllers/Controller.java
+++ src/main/java/br/com/jvsiqueira/crudmysql/controllers/Controller.java
@@ -1,12 +1,31 @@
 package br.com.jvsiqueira.crudmysql.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.jvsiqueira.crudmysql.model.Usuario;
+import br.com.jvsiqueira.crudmysql.services.UsuarioService;
 
 @org.springframework.stereotype.Controller
+@RequestMapping("/")
 public class Controller {
 	
-	@GetMapping(path = "/teste/")
+	@Autowired
+	UsuarioService usuarioService;
+	
+	@GetMapping(path = "teste")
 	public void teste() {
 		System.out.println("Chegou aqui");
+	}
+	
+	@GetMapping(path = "usuarios")
+	public String listaUsuario(Model model) {
+		List<Usuario> lista = usuarioService.consultaTodos(); 
+		model.addAttribute("userList", lista);
+		return "usuarios";
 	}
 }
--- src/main/java/br/com/jvsiqueira/crudmysql/controllers/UsuarioController.java
+++ src/main/java/br/com/jvsiqueira/crudmysql/controllers/UsuarioController.java
@@ -3,7 +3,7 @@ package br.com.jvsiqueira.crudmysql.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,17 +11,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jvsiqueira.crudmysql.model.Usuario;
+import br.com.jvsiqueira.crudmysql.repositories.UsuarioRepository;
 import br.com.jvsiqueira.crudmysql.services.UsuarioService;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class UsuarioController {
 	
 	@Autowired
 	UsuarioService usuarioService;
-	
+
 	@GetMapping(path = "consulta")
 	public List<Usuario> consultaTodos() {
 		List<Usuario> lista = usuarioService.consultaTodos();
